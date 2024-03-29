4c99f615fff5de060ff193fc973b1308737fb567
Emma Chang <yihsienchang@Emmas-Laptop.local>
2024-03-29 14:58:16 -0400

    HW2 - before

--- profile.R
+++ profile.R
@@ -1,7 +1,20 @@
-x = matrix(runif(1e5*500), ncol = 500)
+euclidean_distance = function(x1, y1, x2, y2) {
+  distance = sqrt((x2 - x1)^2 + (y2 - y1)^2)
+  return(distance)
+}
+
+set.seed(123) # Setting seed for reproducibility
+num_points <- 10^6
+x1 = runif(num_points)
+y1 = runif(num_points)
+x2 = runif(num_points)
+y2 = runif(num_points)
 
  
 Rprof()
-x_pr = prcomp(x, retx = FALSE)
+distances = numeric(num_points)
+  for (i in 1:num_points) {
+    distances[i] = euclidean_distance(x1[i], y1[i], x2[i], y2[i])
+  }
 Rprof(NULL)
 summaryRprof()
